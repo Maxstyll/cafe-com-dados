cbfcc01575aebb8e8c863354b5148a8bd6e0d104
Emerson Antonio <afirmanet@gmail.com>
2021-06-05 10:12:39 -0300

    Add PCA

--- Python_TensorFlow/src/main.py
+++ Python_TensorFlow/src/main.py
@@ -13,17 +13,9 @@ class Main:
         5: Callbacks()
     }
 
-    switcherPytorch = {
-        1: "Invalid month"
-    }
-
     def __init__(self):
         pass
     
     def tensorflow(self, nunberAula):
         aula = self.switcherTensorflow.get(nunberAula, "Invalid month")
         aula.run()
-
-    def pytorch(self, nunberAula):
-        aula = self.switcherPytorch.get(nunberAula, "Invalid month")
-        print(aula)
