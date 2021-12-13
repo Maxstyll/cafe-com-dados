5cbeef31942954d8749cc7e43cd89f0cfb0aa550
Emerson Antonio <afirmanet@gmail.com>
2021-12-13 13:34:37 -0300

    feat: update filr __init__.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-__version__ = "0.2.0"
-
-setup(
-   name="café-com-dados",
-   version=__version__,
-   # And so on...
-)
--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,8 @@
+from setuptools import setup
 __version__ = '0.3.0'
+
+setup(
+   name="café-com-dados",
+   version=__version__,
+   # And so on...
+)
